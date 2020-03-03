2d6984237b909b256b1c2198629afe7e755fbf0a
Martin B. Hansen <mabh@eaaa.dk>
2020-03-03 09:18:47 +0100

    Added functionality to extended file handling

--- services/os/Operations.py
+++ services/os/Operations.py
@@ -19,3 +19,30 @@ def is_dir(path):
 
 def makedirs(path, override=False):
     os.makedirs(path, exist_ok=override)
+
+def copy_file(res_path, copy_name):
+    res_path_splitted = res_path.split("\\")
+
+    copy_path = ""
+
+    for x in range(0, len(res_path_splitted)- 1):
+        copy_path = path_join(copy_path, res_path_splitted[x])
+    
+    copy_path = path_join(copy_path, copy_name)
+
+    with open(res_path) as original:
+        with open(copy_path, 'w') as copy:
+            copied = original.readline()
+
+            while copied != "":
+                copy.write(copied)
+
+                copied = original.readline()
+    
+    return copy_path
+
+def remove_file(path):
+    os.remove(path)
+
+def remove_dir(path):
+    os.rmdir(path)
