4f8a9131cf021a225547c6c4c30992a176c233e7
Wickoe <martin@behrens-hansen.dk>
2020-04-22 12:36:32 +0200

    For evaluation of Apple Watch data

---
+++ ML/tasks/AppleDataEvaluateTask.py
@@ -0,0 +1,8 @@
+import services.pipelines.Task as task
+
+class AppleDataEvaluateTask(task.Task):
+    def exec(self, task_input, task_output):
+        pass
+    
+    def reverse(self, task_input, task_output):
+        pass
